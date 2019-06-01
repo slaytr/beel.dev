67ad61e5b7d98b64c88d86da317cb555e2fe9673
Bill Li <slaytraz@gmail.com>
2019-06-02 01:28:21 +1200

    Routes, Router, NavLinks added

--- portfolio/src/components/app.js
+++ portfolio/src/components/app.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Route, Link, Switch, BrowserRouter as Router } from 'react-router-dom'
 import Resume from './resume';
 import TechStack from './techstack';
 import Projects from './projects';
@@ -12,69 +13,22 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      navState: "dev",
       barMenu: "hide",
       height: window.innerHeight,
-      width: window.innerWidth
+      width: window.innerWidth,
+      pages: ["story", "resume", "stack", "projects", "dev"],
     }
   }
 
   componentDidMount() {
     console.log(this.state.height);
     window.addEventListener("resize", this.updateDimensions);
-    window.onpopstate = (event) => {
-      console.log(window.history);
-      console.log(this.state.navState);
-      console.log(event);
-    }
   }
 
   updateDimensions = () => {
     this.setState({height: window.innerHeight, width: window.innerWidth});
   }
 
-  handleClickResume = () => {
-    this.setState(state => ({navState: "resume"}));
-    window.scrollTo(0, 0);
-    window.history.replaceState(null, null, 'resume');
-    this.setState(state => ({barMenu: "hide"}));
-    window.history.pushState(this.state,  null, "resume");
-
-    console.log(window.history.state);
-    console.log("pushed resume");
-  };
-  handleClickStack = () => {
-    this.setState(state => ({navState: "stack"}));
-    window.scrollTo(0, 0);
-    window.history.replaceState(null, null, 'stack');
-    this.setState(state => ({barMenu: "hide"}));
-
-    console.log(window.history)
-
-    window.history.pushState(this.state, null, "stack");
-
-    console.log(window.history.state);
-    console.log(window.history);
-    console.log("pushed stack");
-  };
-  handleClickStory = () => {
-    this.setState(state => ({navState: "story"}));
-    window.scrollTo(0, 0);
-    window.history.replaceState(null, null, 'story');
-    this.setState(state => ({barMenu: "hide"}));
-  };
-  handleClickProjects = () => {
-    this.setState(state => ({navState: "projects"}));
-    window.scrollTo(0, 0);
-    window.history.replaceState(null, null, 'projects');
-    this.setState(state => ({barMenu: "hide"}));
-  };
-  handleClickDev = () => {
-    this.setState(state => ({navState: "dev"}));
-    window.scrollTo(0, 0);
-    window.history.replaceState(null, null, 'dev');
-    this.setState(state => ({barMenu: "hide"}));
-  };
   handleClickMenu = () => {
     if (this.state.barMenu === "hide"){
     this.setState(state => ({barMenu: "show"}));
@@ -85,50 +39,32 @@ class App extends React.Component {
 
   render() {
 
-    let sectionPage;
-    switch(this.state.navState) {
-        case 'story':
-          sectionPage = <MyStory onClick={this.showDropdownMenu} />;
-          break;
-        case 'resume':
-          sectionPage = <Resume clickStack={this.handleClickStack}/>;
-          break;
-        case 'stack':
-          sectionPage = <TechStack />;
-          break;
-        case 'projects':
-          sectionPage = <Projects />;
-          break;
-        case 'dev':
-          sectionPage = <Dev />;
-          break;
-        default:
-          sectionPage = <div> 404 How did you get here? </div>
-      }
-
     return (
-      <div className="Portfolio">
-        <NavMenu
-          screenWidth={this.state.width}
-          barMenu={this.state.barMenu}
-          menuState={this.state.navState}
+        <Router >
+        <div className="Portfolio">
 
-          handleClickMenu={this.handleClickMenu}
-          handleClickStory={this.handleClickStory}
-          handleClickStack={this.handleClickStack}
-          handleClickResume={this.handleClickResume}
-          handleClickProjects={this.handleClickProjects}
-          handleClickDev={this.handleClickDev}
-         />
+          <NavMenu
+            screenWidth={this.state.width}
+            barMenu={this.state.barMenu}
+            handleClickMenu={this.handleClickMenu}/>
 
-      <section className="PortfolioPage">
-        {sectionPage}
-      </section>
+          <section className="PortfolioPage">
+            <Route path="/" component={MyStory} exact/>
 
-      <Footer />
+            <Route path="/MyStory" component={MyStory} exact/>
+            <Route path="/Resume" component={Resume} exact/>
+            <Route path="/MyStack" component={TechStack} exact/>
+            <Route path="/Projects" component={Projects} exact/>
+            <Route path="/Dev" component={Dev} exact/>
+          </section>
 
-    </div>);
+          <Footer />
+
+        </div>
+        </Router>
+  );
   }
+
   componentWillUnmount() {
     window.removeEventListener("resize", this.updateDimensions);
   }
--- portfolio/src/components/nav.js
+++ portfolio/src/components/nav.js
@@ -1,83 +1,83 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import './nav.css';
 
 function NavMenu(props) {
-
-  let storyItem = "menu-item";
-  let resumeItem = "menu-item";
-  let stackItem = "menu-item";
-  let projectsItem = "menu-item";
-  let devItem = "menu-item";
-
-  switch(props.menuState) {
-    case 'story':
-      storyItem = storyItem + " active"
-      break;
-    case 'resume':
-      resumeItem = resumeItem + " active"
-      break;
-    case 'stack':
-      stackItem = stackItem + " active"
-      break;
-    case 'projects':
-      projectsItem = projectsItem + " active"
-      break;
-    case 'dev':
-      devItem = devItem + " active"
-      break;
-  }
-  return (<navbar>
+  return (
+    <navbar>
     {
       props.screenWidth > 600
-        ? (<ul className="topbar">
-          <li className={storyItem} onClick={props.handleClickStory}>My Story</li>
-          <li className={resumeItem} onClick={props.handleClickResume}>Resume</li>
-          <li className={stackItem} onClick={props.handleClickStack}>My Stack</li>
-          <li className={projectsItem} onClick={props.handleClickProjects}>Projects</li>
-          <li className={devItem} onClick={props.handleClickDev}>Dev</li>
-        </ul>)
-        : (<ul className="topbar">
-          <li className="menu-item" style={{
+        ? (
+        <div className="topbar">
+          <NavLink to="/MyStory"
+            activeClassName="active"
+            className="menu-item">
+            My Story
+          </NavLink>
+          <NavLink to="/Resume"
+            activeClassName="active"
+            className="menu-item">
+            Resume
+          </NavLink>
+          <NavLink to="/MyStack"
+            activeClassName="active" 
+            className="menu-item">
+            My Stack
+          </NavLink>
+          <NavLink to="/Projects"
+            activeClassName="active"
+            className="menu-item">
+            Projects
+          </NavLink>
+          <NavLink to="/Dev"
+            activeClassName="active"
+            className="menu-item">
+            Dev
+          </NavLink>
+        </div>)
+        : (
+        <div className="topbar">
+          <div className="menu-item" style={{
               float: "right"
             }} onClick={props.handleClickMenu}>
             <i class="fas fa-bars" style={{
                 paddingLeft: 10,
                 paddingRight: 10
               }}></i>
-          </li>
-          <li className={props.menuState === "story"
-              ? "active menu-item " + props.barMenu
-              : "menu-item " + props.barMenu} style={{
-              float: "none",
-              textAlign: "left"
-            }} onClick={props.handleClickStory}>My Story</li>
-          <li className={props.menuState === "resume"
-              ? "active menu-item " + props.barMenu
-              : "menu-item " + props.barMenu} style={{
-              float: "none",
-              textAlign: "left"
-            }} onClick={props.handleClickResume}>Resume</li>
-          <li className={props.menuState === "stack"
-              ? "active menu-item " + props.barMenu
-              : "menu-item " + props.barMenu} style={{
-              float: "none",
-              textAlign: "left"
-            }} onClick={props.handleClickStack}>My Stack</li>
-          <li className={props.menuState === "projects"
-              ? "active menu-item " + props.barMenu
-              : "menu-item " + props.barMenu} style={{
-              float: "none",
-              textAlign: "left"
-            }} onClick={props.handleClickProjects}>Projects</li>
-          <li className={props.menuState === "dev"
-              ? "active menu-item " + props.barMenu
-              : "menu-item " + props.barMenu} style={{
-              float: "none",
-              textAlign: "left"
-            }} onClick={props.handleClickDev}>Dev</li>
-        </ul>)
+          </div>
+          <NavLink to="/MyStory"
+            activeClassName="active"
+            className={"menu-item " + props.barMenu + " mobile"}
+            onClick={props.handleClickMenu}>
+            My Story
+          </NavLink>
+          <NavLink to="/Resume"
+            activeClassName="active"
+            className={"menu-item " + props.barMenu + " mobile"}
+            onClick={props.handleClickMenu}>
+            Resume
+          </NavLink>
+          <NavLink to="/MyStack"
+            activeClassName="active"
+            className={"menu-item " + props.barMenu + " mobile"}
+            onClick={props.handleClickMenu}>
+            My Stack
+          </NavLink>
+          <NavLink to="/Projects"
+            activeClassName="active"
+            className={"menu-item " + props.barMenu + " mobile"}
+            onClick={props.handleClickMenu}>
+            Projects
+          </NavLink>
+          <NavLink to="/Dev"
+            activeClassName="active"
+            className={"menu-item " + props.barMenu + " mobile"}
+            onClick={props.handleClickMenu}>
+            Dev
+          </NavLink>
+        </div>)
     }
-  </navbar>)
+    </navbar>)
 }
 
 export default NavMenu;
