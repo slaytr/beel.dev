80e7b6892f17c2aa16bb1bdd04427fcab0dbb814
Bill Li <slaytraz@gmail.com>
2019-06-04 22:08:35 +1200

    DevCards set, Project proto

--- portfolio/src/components/dev.js
+++ portfolio/src/components/dev.js
@@ -7,39 +7,15 @@ class Dev extends React.Component {
       <div style={{maxWidth: 1400}}>
         <div className="devBox">
           <div className="devCard">
-            <p> hello </p>
+            <p>04/06/19 // Changed state and navigation handling to use React Router</p>
+            <p>Rearchitectured code, huge reduction in redundant code.</p>
           </div>
-        </div>
-        <div class="w3-row-padding">
-          <div class="w3-rest-padding">
-            <div class="w3-container w3-card w3-white w3-margin-bottom devCard" style={{textAlign: "left"}}>
-              <h2 class="w3-text-dark-grey w3-padding-16">History API</h2>
-                <h5 class="w3-text-grey">
-                  <b>Progressive Web App Requirement<div className="weblink">
-                      <a href="https://www.nyriad.com">Use History API according to changes in state</a>
-                    </div>
-                  </b>
-                </h5>
-                <h6 class="w3-text-green">
-                  <i class="fa fa-calendar fa-fw w3-margin-right"></i>31/05/19</h6>
-                <p>Areas affected: My Story, Resume, My Stack, Projects, Dev. Added features, URL extension for appropriate state change when corresponding events are engaged.</p>
-            </div>
+          <div className="devCard">
+            <p>02/06/19 // Applied history API </p>
+            <p>Non-AJAX navigation implemented. Code needs tidy up.</p>
           </div>
-        </div>
-        <div class="w3-row-padding">
-          <div class="w3-rest-padding">
-            <div class="w3-container w3-card w3-white w3-margin-bottom devCard" style={{textAlign: "left"}}>
-              <h2 class="w3-text-dark-grey w3-padding-16">Mobile Dropdown Menu</h2>
-                <h5 class="w3-text-grey">
-                  <b>Responsive Web Design Requirement<div className="weblink">
-                      <a href="https://www.nyriad.com">Change Navbar User Interface according to screen width</a>
-                    </div>
-                  </b>
-                </h5>
-                <h6 class="w3-text-green">
-                  <i class="fa fa-calendar fa-fw w3-margin-right"></i>30/05/19</h6>
-                <p>Areas affected: Navbar. Added features, bars icon added to navbar when the width of the screen is tablet or mobile. </p>
-            </div>
+          <div className="devCard">
+            <p>26/05/19 // Initial Release</p>
           </div>
         </div>
       </div>
--- portfolio/src/components/projects.js
+++ portfolio/src/components/projects.js
@@ -1,16 +1,72 @@
 import React from 'react';
+import './projects.css';
 
 class Projects extends React.Component {
   render() {
     return (
-      <div class="w3-content" style={{maxWidth:1400}}>
-      <div className="w3-row">
-        <div className="w3-rest">
-        <div className="w3-white w3-card-1 w3-text-dark-grey">
-        <div className="w3-container"><h1>In Development</h1></div>
+      <div style={{maxWidth: 1400}}>
+        <div className="ProjectBox">
+          <div className="ProjectCard">
+          <div className="ProjectCardImage">
+          Two Sum
+          </div>
+          <div className="ProjectCardText">
+            Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+            <br/>
+            You may assume that each input would have exactly one solution, and you may not use the same element twice.
+          </div>
+          </div>
+          <div className="ProjectCard">
+          <div className="ProjectCardImage">
+          Hello
+          </div>
+          <div className="ProjectCardText">
+            Changed state and navigation handling to use React Router
+            <br/>
+            Rearchitectured code, huge reduction in redundant code.
+          </div>
+          </div>
+          <div className="ProjectCard">
+          <div className="ProjectCardImage">
+          Hello
+          </div>
+          <div className="ProjectCardText">
+            Changed state and navigation handling to use React Router
+            <br/>
+            Rearchitectured code, huge reduction in redundant code.
+          </div>
+          </div>
+          <div className="ProjectCard">
+          <div className="ProjectCardImage">
+          Hello
+          </div>
+          <div className="ProjectCardText">
+            Changed state and navigation handling to use React Router
+            <br/>
+            Rearchitectured code, huge reduction in redundant code.
+          </div>
+          </div>
+          <div className="ProjectCard">
+          <div className="ProjectCardImage">
+          Hello
+          </div>
+          <div className="ProjectCardText">
+            Changed state and navigation handling to use React Router
+            <br/>
+            Rearchitectured code, huge reduction in redundant code.
+          </div>
+          </div>
+          <div className="ProjectCard">
+          <div className="ProjectCardImage">
+          Hello
+          </div>
+          <div className="ProjectCardText">
+            Changed state and navigation handling to use React Router
+            <br/>
+            Rearchitectured code, huge reduction in redundant code.
+          </div>
+          </div>
         </div>
-        </div>
-      </div>
       </div>
     )
   }
