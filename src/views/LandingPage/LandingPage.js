cc65d24c9c70642e68a73cc5cb085f42d03901ad
Bill Li <cl337@students.waikato.ac.nz>
2021-05-22 01:30:23 +1200

    exploration to ui kit

--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -34,7 +34,7 @@ export default function LandingPage(props) {
       <Header
         color="transparent"
         routes={dashboardRoutes}
-        brand="Material Kit React"
+        brand="beel.dev"
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
@@ -47,12 +47,9 @@ export default function LandingPage(props) {
         <div className={classes.container}>
           <GridContainer>
             <GridItem xs={12} sm={12} md={6}>
-              <h1 className={classes.title}>Your Story Starts With Us.</h1>
+              <h1 className={classes.title}>A Solutions Architect For Your Needs.</h1>
               <h4>
-                Every landing page needs a small description after the big bold
-                title, that{"'"}s why we added this text here. Add here all the
-                information that can make you or your product create the first
-                impression.
+                Creating software from the ground up and turning your ideas from dreams to reality.
               </h4>
               <br />
               <Button
